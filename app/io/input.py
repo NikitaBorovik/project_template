4787137e2ca5c7e59bdf9693812ec35e193f3b42
Нікіта Боровік <nikita.borovik@ukma.edu.ua>
2024-03-19 21:03:05 +0200

    added empty input/output functions

---
+++ app/io/input.py
@@ -0,0 +1,12 @@
+
+
+def read_text_console():
+    pass
+
+
+def read_file_python():
+    pass
+
+
+def read_file_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,12 @@
+
+
+def write_text_console():
+    pass
+
+
+def write_file_python():
+    pass
+
+
+def write_file_pandas():
+    pass
